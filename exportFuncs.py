c89081f39bc9201b72e702a0a421ad8e8fbcb3f0
Timo <timo@debian-BULLSEYE-live-builder-AMD64>
2023-06-06 14:34:22 +0200

    fixing exportFuncs.py

--- exportFuncs.py
+++ exportFuncs.py
@@ -1,9 +1,11 @@
 
-from .operators import get_U, get_La, get_Ra, get_Lsquared
+from .operators import get_U, get_Lsquared, get_La  # , get_Ra
 
 from .DJT_matrix import get_V, get_V_inv, get_DJT
 
 import numpy as np
+
+from scipy.sparse import bmat
 
 
 def get_partition_list(q):
@@ -13,10 +15,10 @@ def get_partition_list(q):
 
     out = np.zeros((partSize, 4))
 
-    out[:0] = U[0][0].diagonal().real()
-    out[:, 1] = U[0][0].diagonal().imag()
-    out[:, 2] = U[0][1].diagonal().real()
-    out[:, 3] = U[0][1].diagonal().imag()
+    out[:, 0] = np.real(np.diagonal(U[0][0]))
+    out[:, 1] = np.imag(np.diagonal(U[0][0]))
+    out[:, 2] = np.real(np.diagonal(U[0][1]))
+    out[:, 3] = np.imag(np.diagonal(U[0][1]))
 
     return out
 
@@ -34,8 +36,13 @@ def get_canonical_momenta(q):
 
     Lsq = get_Lsquared(q, V, V_inv)
 
-    R1 = get_Ra(1, q, V, V_inv)
-    R2 = get_Ra(2, q, V, V_inv)
-    R3 = get_Ra(3, q, V, V_inv)
+    R1 = get_La(1, q, V, V_inv)
+    R2 = get_La(2, q, V, V_inv)
+    R3 = get_La(3, q, V, V_inv)
 
-    return Lsq, L1, L2, L3, R1, R2, R3
+
+    # out = [bmat([[Lsq]], format="csc"), bmat([[L1]], format="csc"), bmat([[L2]], format="csc"), bmat([[L3]], format="csc"), bmat([[R1]], format="csc"), bmat([[R2]], format="csc"), bmat([[R3]], format="csc")]
+
+    out = [Lsq, L1, L2, L3, R1, R2, R3]
+
+    return out
