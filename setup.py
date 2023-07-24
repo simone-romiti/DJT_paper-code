8b75270c338e1800905f5b0ed96d8d6575530782
simone-romiti <simone.romiti.1994@gmail.com>
2023-07-24 10:42:28 +0200

    su2_DJT can now be installed with pip: see `install_su2_DJT.sh`

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='su2_DJT',
+    version='1.0',
+    description='SU(2) canonical momenta from the DJT',
+    url='https://github.com/simone-romiti/su2_discrete-code',
+    author='Simone Romiti',
+    author_email='simone.romiti.1994@gmail.com',
+    packages=find_packages(),
+)
--- su2_DJT/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='su2_DJT',
-    version='1.0',
-    packages=find_packages(),
-)
