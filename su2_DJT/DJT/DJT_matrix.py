fixed normalization of Wigner D functions. Now it's consistent with the DJT paper

su2_DJT/DJT/DJT_matrix.py
@@ -11,11 +11,19 @@ from su2_DJT.S3_sphere.indices import *
 
 
 def get_WignerD(j, mL, mR, alpha, beta, gamma, use_sympy):
+    """
+    Returns the Wigner D function:
+
+    D^{j}_{mL,mR} = e^{+i*alpha} * d^{j}_{mL,mR}(beta) * e^{+i*gamma}
+    
+    Note: the factor (-1)^(mL-mR) is needed to compensate the different normalization 
+
+    """
     if(j < 29 and not use_sympy): # spherical_functions package is stable up to this point
-        # complex conjugate of the value obtained with sympy
-        return np.conj(spherical_functions.Wigner_D_element(alpha, beta, gamma, j, mL, mR))
+        # complex conjugate to get the same as sympy
+        return (-1)**(mL-mR) * np.conj(spherical_functions.Wigner_D_element(alpha, beta, gamma, j, -mL, -mR))
     else:
-        return complex(WignerD(sp.Rational(j), sp.Rational(mL), sp.Rational(mR), alpha, beta, gamma).doit())
+        return (-1)**(mL-mR) * complex(WignerD(sp.Rational(j), -sp.Rational(mL), -sp.Rational(mR), alpha, beta, gamma).doit())
 ####
 
 def get_DJT(q, use_sympy=False):

test/test-DJT.py
@@ -12,7 +12,7 @@ from su2_DJT.DJT.DJT_matrix import *
 decimals = 14
 print("rounding decimals", decimals)
 
-q = 1 # truncation (integer or half integer)
+q = 3/2 # truncation (integer or half integer)
 print("q =", q)
 
 N_q = partition.get_N_q(q)
@@ -28,9 +28,8 @@ DJT_dag = get_DJT_dag(DJT = DJT)
 res = DJT_dag * DJT
 
 res = np.array(sp.Matrix(res).evalf(chop=True), dtype=complex).round(decimals=decimals)
-res = np.array(res, dtype=float)
 print(N_q, res.shape)
 
 print("DJT^\dagger * DJT")
-print(res)
+print(np.array_equal(res, np.eye(N_q, dtype=complex)))
 